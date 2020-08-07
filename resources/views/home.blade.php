648bd780c2e0528d8d1e3bcd031256ea65def1de
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-08-07 10:28:26 -0400

    feat: Login page mockup
    Also added and integrated username attribute to the database in home, app, user model and migration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div class="container">
+        <div class="row">
+            <div class="col-3 p-5">
+                <img
+                    src="https://instagram.fyhu2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.fyhu2-1.fna.fbcdn.net&amp;_nc_ohc=LFHchPCFeR0AX_jYfOT&amp;oh=c94ec2a2f75789c25cee3aa04abcdb56&amp;oe=5F56A067"
+                    class="rounded-circle">
+            </div>
+            <div class="col-9 pt-5">
+                <div><h1>freecodecamp</h1></div>
+                <div class="d-flex">
+                    <div class="pr-5"><strong>153</strong> posts</div>
+                    <div class="pr-5"><strong>23k</strong> followers</div>
+                    <div class="pr-5"><strong>212</strong> following</div>
                 </div>
+                <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+                <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+                <div><a href="#">www.freecodecamp.org</a></div>
+            </div>
+        </div>
+
+        <div class="row pt-5">
+            <div class="col-4">
+                <img src="https://instagram.fyhu2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.117.937.937a/s640x640/116792589_940704333073490_3603561987847571728_n.jpg?_nc_ht=instagram.fyhu2-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=Gk_inhSY1K0AX9fYwEU&oh=78de1e41951c12c3d7361c883cfb2bbc&oe=5F548833" class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="https://instagram.fyhu2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.746.746a/s640x640/116046434_2514443215325780_2721033236601132108_n.jpg?_nc_ht=instagram.fyhu2-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=McnI0rU6LjIAX_cdjle&oh=7f144c9fabe360174274aadcfb67693d&oe=5F54EFD1" class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="https://instagram.fyhu2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.746.746a/s640x640/116362545_618124912453256_2781481867424758391_n.jpg?_nc_ht=instagram.fyhu2-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=jjWzZngTc0EAX8LvPOo&oh=2494700ab3f217036514f4eb6bba1f66&oe=5F55E8EA" class="w-100">
             </div>
         </div>
     </div>
-</div>
 @endsection
