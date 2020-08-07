b7478c573fac2564143c0852acfa85b0e353b0cf
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-08-07 12:48:02 -0400

    feat: Added Posts Model
    Including:
    - PostsController
    - View first steps
    - Migration for database
    - Links with other models

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,7 +9,10 @@
                     class="rounded-circle">
             </div>
             <div class="col-9 pt-5">
-                <div><h1>{{ $user->username }}</h1></div>
+                <div class="d-flex justify-content-between align-items-baseline">
+                    <h1>{{ $user->username }}</h1>
+                    <a href="#">Add New Post</a>
+                </div>
                 <div class="d-flex">
                     <div class="pr-5"><strong>153</strong> posts</div>
                     <div class="pr-5"><strong>23k</strong> followers</div>
@@ -23,13 +26,19 @@
 
         <div class="row pt-5">
             <div class="col-4">
-                <img src="https://instagram.fyhu2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.117.937.937a/s640x640/116792589_940704333073490_3603561987847571728_n.jpg?_nc_ht=instagram.fyhu2-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=Gk_inhSY1K0AX9fYwEU&oh=78de1e41951c12c3d7361c883cfb2bbc&oe=5F548833" class="w-100">
+                <img
+                    src="https://instagram.fyhu2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.117.937.937a/s640x640/116792589_940704333073490_3603561987847571728_n.jpg?_nc_ht=instagram.fyhu2-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=Gk_inhSY1K0AX9fYwEU&oh=78de1e41951c12c3d7361c883cfb2bbc&oe=5F548833"
+                    class="w-100">
             </div>
             <div class="col-4">
-                <img src="https://instagram.fyhu2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.746.746a/s640x640/116046434_2514443215325780_2721033236601132108_n.jpg?_nc_ht=instagram.fyhu2-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=McnI0rU6LjIAX_cdjle&oh=7f144c9fabe360174274aadcfb67693d&oe=5F54EFD1" class="w-100">
+                <img
+                    src="https://instagram.fyhu2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.746.746a/s640x640/116046434_2514443215325780_2721033236601132108_n.jpg?_nc_ht=instagram.fyhu2-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=McnI0rU6LjIAX_cdjle&oh=7f144c9fabe360174274aadcfb67693d&oe=5F54EFD1"
+                    class="w-100">
             </div>
             <div class="col-4">
-                <img src="https://instagram.fyhu2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.746.746a/s640x640/116362545_618124912453256_2781481867424758391_n.jpg?_nc_ht=instagram.fyhu2-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=jjWzZngTc0EAX8LvPOo&oh=2494700ab3f217036514f4eb6bba1f66&oe=5F55E8EA" class="w-100">
+                <img
+                    src="https://instagram.fyhu2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.746.746a/s640x640/116362545_618124912453256_2781481867424758391_n.jpg?_nc_ht=instagram.fyhu2-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=jjWzZngTc0EAX8LvPOo&oh=2494700ab3f217036514f4eb6bba1f66&oe=5F55E8EA"
+                    class="w-100">
             </div>
         </div>
     </div>
