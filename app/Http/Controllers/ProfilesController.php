4b80a3bd86e217a37e5240f3f20d9430ba7d42f5
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-08-10 14:19:49 -0400

    feat: Profile image update
    - When a user creates a new account, a default image is used.
    - When the user edits its profile, checks if an image has been uploaded and updates/delete the other image when needed.

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -32,18 +32,20 @@ class ProfilesController extends Controller
            'image'=> '',
         ]);
 
-
+        // If there is an image in the request
         if (request('image')) {
             $imagePath = request('image')->store('profile', 'public');
 
             $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
             $image->save();
+
+            $imageArray = ['image' => $imagePath]; // overrides what was stored in $data for 'image'
         }
 
         // auth() add an extra level of protection to make sure an unregistred user can't change another user's profile
         auth()->user()->profile->update(array_merge(
             $data,
-            ['image' => $imagePath] // overrides what was stored in $data for 'image'
+            $imageArray ?? []   // if imageArray is not set, it defaults to an empty array to avoid deleting image when none is uploaded
         ));
 
         return redirect("/profile/{$user->id}");
--- app/Profile.php
+++ app/Profile.php
@@ -10,6 +10,12 @@ class Profile extends Model
     // disabling out of the box protection to allow update in ProfilesController
     protected $guarded = [];
 
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : '/profile/avatar.jpg';
+        return '/storage' . $imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
